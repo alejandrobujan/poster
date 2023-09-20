2ad523e93d10c55203b4b542da94ebe6f2145283
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-09-20 12:20:12 +0200

    Created PostDao

---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/PostDao.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.fd.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface PostDao.
+ */
+public interface PostDao extends JpaRepository<Post, Long> {
+
+}
