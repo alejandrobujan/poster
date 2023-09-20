cb71bbad7005107b98586cc6697c6cf0e3a36662
martinregueirogolan <martin.regueiro1@udc.es>
2023-09-20 12:54:01 +0200

    Modified ImageDao

--- src/main/java/es/udc/fi/dc/fd/model/entities/ImageDao.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/ImageDao.java
@@ -3,9 +3,12 @@
  */
 package es.udc.fi.dc.fd.model.entities;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
- * 
+ * The Interface ImageDao.
  */
-public class ImageDao {
+public interface ImageDao extends JpaRepository<Image, Long>{
 
 }
+
