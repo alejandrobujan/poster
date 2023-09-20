6c3f80e1329cc7fe0787a588e29407b92ed64b27
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-20 17:09:30 +0200

    Added ImageDao

---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/ImageDao.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.fd.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface ImageDao.
+ */
+public interface ImageDao extends JpaRepository<Image, Long> {
+	
+}
