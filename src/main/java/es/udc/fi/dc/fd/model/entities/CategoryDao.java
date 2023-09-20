293c58f7cb482aa1a480cd45f07a149ca94ae6c1
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-20 11:58:29 +0200

    Created CategoryDao and UsrDao

---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/CategoryDao.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.fd.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface CategoryDao.
+ */
+public interface CategoryDao extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/UsrDao.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package es.udc.fi.dc.fd.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface UserDao.
+ */
+public interface UsrDao extends JpaRepository<User, Long>{
+
+}
