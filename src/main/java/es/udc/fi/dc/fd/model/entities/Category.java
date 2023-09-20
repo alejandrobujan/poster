27d18068b5baef444b4de2089bb9eb14b5317b29
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-20 11:26:50 +0200

    Changed Category and User

--- src/main/java/es/udc/fi/dc/fd/model/entities/Category.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/Category.java
@@ -1,22 +1,41 @@
 package es.udc.fi.dc.fd.model.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+/**
+ * The Class Category.
+ */
 @Entity
 public class Category {
 	
+	/**The id.*/
 	private Long id;
+	/**The category name.*/
 	private String name;
 	
-	
 	/**
+	 * Instantiates a new category.
+	 */
+	public Category() {
+	}
+
+	/**
+	 * Gets the id.
+	 * 
 	 * @return the id
 	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
 	
 	/**
+	 * Sets the id.
+	 * 
 	 * @param id the id to set
 	 */
 	public void setId(Long id) {
@@ -24,6 +43,8 @@ public class Category {
 	}
 	
 	/**
+	 * Gets the category name.
+	 * 
 	 * @return the name
 	 */
 	public String getName() {
@@ -31,6 +52,8 @@ public class Category {
 	}
 	
 	/**
+	 * Sets the category name.
+	 * 
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
--- src/main/java/es/udc/fi/dc/fd/model/entities/User.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/User.java
@@ -8,57 +8,118 @@ import jakarta.persistence.Id;
 @Entity
 public class User {
 	
+	/**The id.*/
 	private Long id;
+	/**The login.*/
 	private String login;
+	/**The password.*/
 	private String password;
+	/**The name.*/
 	private String name;
+	/**The avatar url.*/
 	private String avatarUrl;
-	
-	public User () {
-		
+
+	/**
+	 * Instantiates a new user.
+	 */
+	public User() {
 	}
-	
+
+	/**
+	 * Gets the id.
+	 * 
+	 * @return the id
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
-	
+
+	/**
+	 * Sets the id.
+	 * 
+	 * @param id the id to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	/**
+	 * Gets the login.
+	 * 
+	 * @return the login
+	 */
 	public String getLogin() {
 		return login;
 	}
-	
+
+	/**
+	 * Sets the login.
+	 * 
+	 * @param login the login to set
+	 */
 	public void setLogin(String login) {
 		this.login = login;
 	}
 	
+	/**
+	 * Gets the password.
+	 * 
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
-	
+
+	/**
+	 * Sets the password.
+	 * 
+	 * @param password the password to set
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
+	/**
+	 * Gets the name.
+	 * 
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
-	
+
+	/**
+	 * Sets the name.
+	 * 
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * Gets the avatar url.
+	 * 
+	 * @return the avatarUrl
+	 */
 	public String getAvatarUrl() {
 		return avatarUrl;
 	}
-	
+
+	/**
+	 * Sets the avatar url.
+	 * 
+	 * @param avatarUrl the avatarUrl to set
+	 */
 	public void setAvatarUrl(String avatarUrl) {
 		this.avatarUrl = avatarUrl;
 	}
+
+	
+	
+	
 	
 	
 
