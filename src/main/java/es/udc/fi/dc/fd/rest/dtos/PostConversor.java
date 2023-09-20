b94cd4504faa00ee1c495515c2201d0945993a12
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-20 18:20:35 +0200

    Updated PostConversor

--- src/main/java/es/udc/fi/dc/fd/rest/dtos/PostConversor.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/PostConversor.java
@@ -3,28 +3,37 @@
  */
 package es.udc.fi.dc.fd.rest.dtos;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import es.udc.fi.dc.fd.model.entities.Image;
 import es.udc.fi.dc.fd.model.entities.Post;
+
+import static es.udc.fi.dc.fd.rest.dtos.CategoryConversor.toCategoryDto;
 
 /**
  * The class PostConversor.
  */
 public class PostConversor {
 	
-	/**
-	 * Instantiates a new post conversor.
-	 */
-	private PostConversor () {
+	private PostConversor () {}
+	
+	public final static byte[] toImageDto(Image image) {
+		return image.getData();
 	}
 	
-	public static final PostParamsDto toPostParamsDto (Post post) {
-		return new PostParamsDto(post.getId(), post.getTitle(), post.getDescription(),
-				post.getUrl(), post.getPrice(), post.getUser(), post.getCategory(), post.getImages());
+	public final static List<byte[]> toImageDtos(Set<Image> images) {
+		return images.stream().map(i -> toImageDto(i)).collect(Collectors.toList());
 	}
 	
-	public static final Post toPost (PostParamsDto postParamsDto) {
-		return new Post(postParamsDto.getId(), postParamsDto.getTitle(), postParamsDto.getDescription(),
-				postParamsDto.getUrl(), postParamsDto.getPrice(), postParamsDto.getUser(),
-				postParamsDto.getCategory(), postParamsDto.getImages());
+	public final static PostSummaryDto toPostSummaryDto(Post post) {
+		return new PostSummaryDto(post.getId(), post.getTitle(), post.getDescription(), post.getUrl(), post.getPrice(),
+				toCategoryDto(post.getCategory()), toImageDtos(post.getImages()));
 	}
+	
+	public final static List<PostSummaryDto> toPostSummaryDtos(List<Post> posts) {
+        return posts.stream().map(p -> toPostSummaryDto(p)).collect(Collectors.toList());
+    }
 	
 }
