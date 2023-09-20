87172c26ebee1e1eefacc48228be50566f8b99a4
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-20 11:10:52 +0200

    Created entities Category and User

---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/User.java
@@ -0,0 +1,65 @@
+package es.udc.fi.dc.fd.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	
+	private Long id;
+	private String login;
+	private String password;
+	private String name;
+	private String avatarUrl;
+	
+	public User () {
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+	
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+	
+	
+
+}
