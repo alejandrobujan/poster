e54b4c0c41d7160f50170c19734168a3813cbf16
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-20 15:00:34 +0200

    Updated PostDao

--- src/main/java/es/udc/fi/dc/fd/model/entities/PostDao.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/PostDao.java
@@ -1,10 +1,12 @@
 package es.udc.fi.dc.fd.model.entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 /**
  * The Interface PostDao.
  */
 public interface PostDao extends JpaRepository<Post, Long> {
-
+	Slice<Post> findAllByOrderByCreationDateDesc(Pageable pageable);
 }
