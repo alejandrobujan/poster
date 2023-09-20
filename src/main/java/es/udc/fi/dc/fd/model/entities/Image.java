bb3d113d2a6e74e7318ea540cf7302cd159886e8
martinregueirogolan <martin.regueiro1@udc.es>
2023-09-20 12:06:12 +0200

    Modified Image Entity

--- src/main/java/es/udc/fi/dc/fd/model/entities/Image.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/Image.java
@@ -5,48 +5,85 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/**
+ * The Class Image.
+ */
+
 @Entity
 public class Image {
 	
+	/** The image id. */
 	private Long id;
+	
+	/** The movie url. */
 	private String url;
+	
+	/** The id of the post. */
 	private Long postId;
 	
-	
+	/**
+	 * Instantiates a new image.
+	 */
 	public Image() {
 		
 	}
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
 
-
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the id to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
+	/**
+	 * * Gets the url.
+	 *
+	 * @return the url
+	 */
 	public String getUrl() {
 		return url;
 	}
 
-
+	/**
+	 * Sets the url.
+	 *
+	 * @param url the url to set
+	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
 
-
+	/**
+	 * Gets the postId.
+	 *
+	 * @return the postId
+	 */
 	public Long getPostId() {
 		return postId;
 	}
 
-
+	/**
+	 * Sets the postId.
+	 *
+	 * @param postId the postId to set
+	 */
 	public void setPostId(Long postId) {
 		this.postId = postId;
 	}
+
 	
 	
 	
