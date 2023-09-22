bb5a0258381bdc04d6aba68a416223564c4ffafd
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-22 17:34:40 +0200

    Added users to rootReducer

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,12 +1,12 @@
 import {combineReducers} from 'redux';
 
 import app from '../modules/app';
-//import users from '../modules/users';
+import users from '../modules/users';
 import post from '../modules/post';
 
 const rootReducer = combineReducers({
     app: app.reducer,
-    //users: users.reducer,
+    users: users.reducer,
     post: post.reducer
 });
 
