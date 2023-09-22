e0a47c3c4979ed755014708a3f9b2f7b066e3c0b
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-22 13:08:39 +0200

    Created store and modified global index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+
+import {BrowserRouter} from 'react-router-dom';
+
+import store from './store';
+
+import backend from './backend';
+
+import {NetworkError} from './backend';
 
 import { App } from "./modules/app";
+import app from './modules/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+
+import registerServiceWorker from './registerServiceWorker';
+
+
+backend.init(error => store.dispatch(app.actions.error(new NetworkError())));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+	<React.StrictMode>
+        <Provider store={store}>
+             <BrowserRouter>
+                 <App/>
+             </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
+
 registerServiceWorker();
---
+++ frontend/src/store/rootReducer.js
@@ -0,0 +1,13 @@
+import {combineReducers} from 'redux';
+
+import app from '../modules/app';
+//import users from '../modules/users';
+import post from '../modules/post';
+
+const rootReducer = combineReducers({
+    app: app.reducer,
+    //users: users.reducer,
+    post: post.reducer
+});
+
+export default rootReducer;
