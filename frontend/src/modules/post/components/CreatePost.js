2e890c4a74390b262c689cd1b533cc4a653c3ede
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-22 09:47:03 +0200

    Modified component CreatePost.js

--- frontend/src/modules/post/components/CreatePost.js
+++ frontend/src/modules/post/components/CreatePost.js
@@ -12,12 +12,12 @@ const CreatePost = () => {
 	const [title, setTitle] = useState('');
 	const [description, setDescription] = useState('');
 	const [url, setUrl] = useState('');
-	const [price, setPrice] = useState(1);
-	const [categoryId, setCategoryId] = useState(1);
+	const [price, setPrice] = useState(0);
+	const [categoryId, setCategoryId] = useState(0);
 	const [images, setImages] = useState([]);
 	const [backendErrors, setBackendErrors] = useState(null);
 	
-	 const handleSubmit = event => {
+	const handleSubmit = event => {
 
         event.preventDefault();
 
@@ -37,28 +37,100 @@ const CreatePost = () => {
 			 <form ref={node => form = node}
 	                  className="needs-validation" noValidate
 	                  onSubmit={e => handleSubmit(e)}>
-	         	<div className="form-group row">
-                    <label htmlFor="title" className="offset-md-4 col-md-2 col-form-label">
-                        Quantity
+	         	<div>
+                    <label htmlFor="title">
+                        Title
                     </label>
-                    <div className="col-md-4">
-                        <input type="text" className="form-control"
-                               value={quantity}
-                               onChange={e => setQuantity(Number(e.target.value))}
+                    <div>
+                        <input type="text"
+                               value={title}
+                               onChange={e => setTitle(e.target.value)}
                                autoFocus
-                               min="1"
-                               max="10"
+                               minLength={1}
+                               maxLength={60}
+                               required/>
+                         <div className="invalid-feedback">
+                            The title size must be between 1 and 60
+                        </div>       
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <div>
+                        <input type="text"
+                               value={description}
+                               onChange={e => setDescription(e.target.value)}
+                               autoFocus
+                               minLength={1}
+                               maxLength={256}
+                               required/>
+                        <div className="invalid-feedback">
+                            The description size must be between 1 and 256
+                        </div>        
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="url">
+                        Url
+                    </label>
+                    <div>
+                        <input type="text"
+                               value={url}
+                               onChange={e => setUrl(e.target.value)}
+                               autoFocus
+                               minLength={0}
+                               maxLength={2048}
+                               />
+                        <div className="invalid-feedback">
+                            The url size must be between 0 and 2048
+                        </div>       
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="price">
+                        Price
+                    </label>
+                    <div>
+                        <input type="number"
+                               value={price}
+                               onChange={e => setPrice(Number(e.target.value))}
+                               autoFocus
+                               min="0"
                                required
                         />
                         <div className="invalid-feedback">
-                            <FormattedMessage id='project.global.validator.incorrectQuantity'/>
+                            Price must be greater than 0
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="categoryId">
+                        Category id
+                    </label>
+                    <div>
+                        <input type="number"
+                               value={categoryId}
+                               onChange={e => setCategoryId(Number(e.target.value))}
+                               autoFocus/>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="images">
+                        Images
+                    </label>
+                    <div>
+                        <input type="image"
+                               value={images}
+                               onChange={e => setImages(Image(e.target.value))}
+                               autoFocus
+                               required/>
                     </div>
                 </div>
 	         </form>
 	     </div>             
-	 );
-    
-    
-		
+	 );	
 };
+
+export default CreatePost;
