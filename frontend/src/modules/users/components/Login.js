9c8785faaee95cf301f29cc9343721ee54e1dc66
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-09-22 17:47:50 +0200

    Removed import

--- frontend/src/modules/users/components/Login.js
+++ frontend/src/modules/users/components/Login.js
@@ -3,7 +3,7 @@ import {useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
 
-import {Errors} from '../../common';
+
 import * as actions from '../actions';
 
 const Login = () => {
@@ -46,7 +46,6 @@ const Login = () => {
                     Sign up
                 </Link>
             </p>
-            <Errors id="loginErrors" errors={backendErrors} onClose={() => setBackendErrors(null)}/>
             <div className="card bg-light border-dark">
                 <h5 className="card-header">
                     Login
