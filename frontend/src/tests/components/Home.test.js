664d69742cb8940d7448307f465b60edcd895d04
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-23 09:51:58 +0200

    Fixed broken frontend test

--- frontend/src/tests/components/Home.test.js
+++ frontend/src/tests/components/Home.test.js
@@ -1,36 +1,42 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
+import {Provider} from 'react-redux';
 import renderer from "react-test-renderer";
 import Home from "../../modules/app/components/Home";
+import store from "../../store";
 
 describe("Home", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+	it("renders correctly", () => {
+		const tree = renderer
+			.create(
+				<Provider store={store}>
+					<MemoryRouter>
+						<Home />
+					</MemoryRouter>
+				</Provider>
+			)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 
-  it("calculates the value as expected", async () => {
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>
-    );
+	it("calculates the value as expected", async () => {
+		render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<Home />
+				</MemoryRouter>
+			</Provider>
+		);
 
-    const inputText = screen.getByLabelText(/input some text/i);
-    fireEvent.change(inputText, {
-      target: { value: "Sreenzragnf qr qrfraibyirzragb" },
-    });
-    const calculateButton = screen.getByText(/calculate/i);
-    fireEvent.click(calculateButton);
-    await waitFor(() => {
-      screen.getByText(/Ferramentas de desenvolvemento/);
-    });
-  });
+		const inputText = screen.getByLabelText(/input some text/i);
+		fireEvent.change(inputText, {
+			target: { value: "Sreenzragnf qr qrfraibyirzragb" },
+		});
+		const calculateButton = screen.getByText(/calculate/i);
+		fireEvent.click(calculateButton);
+		await waitFor(() => {
+			screen.getByText(/Ferramentas de desenvolvemento/);
+		});
+	});
 });
