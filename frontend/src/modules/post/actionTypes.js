6970b40d2382d0e5dd1cc62d1794c48949dfc6a5
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-09-25 11:09:42 +0200

    Modified post actionTypes, actions and index for using CategorySelector

--- frontend/src/modules/post/actionTypes.js
+++ frontend/src/modules/post/actionTypes.js
@@ -1,2 +1,3 @@
 export const CLEAR_POST_SEARCH = "poster/post/clearPostSearch";
 export const FIND_POSTS_COMPLETED = "poster/post/findPostsCompleted";
+export const FIND_ALL_CATEGORIES_COMPLETED = "poster/post/findAllCategoriesCompleted";
