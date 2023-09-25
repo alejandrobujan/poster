d15940ce4d884895d4b4c0550f9d075d9a3450ff
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-25 18:16:43 +0200

    Deleted unnecessary components and its references

--- frontend/src/modules/post/index.js
+++ frontend/src/modules/post/index.js
@@ -3,8 +3,6 @@ import reducer from './reducer';
 import * as selectors from './selectors';
 
 export {default as CreatePost} from './components/CreatePost';
-export {default as FindPosts} from './components/FindPosts';
-export {default as FindPostsResult} from './components/FindPostsResult';
 export {default as Post} from './components/Post';
 export {default as Posts} from './components/Posts';
 export {default as Feed} from './components/Feed';
--- frontend/src/modules/post/selectors.js
+++ frontend/src/modules/post/selectors.js
@@ -1,7 +1,4 @@
 const getModuleState = state => state.post;
 
-export const getPosts = state =>
-    getModuleState(state).posts;
-
 export const getPostSearch = state =>
     getModuleState(state).postSearch;
