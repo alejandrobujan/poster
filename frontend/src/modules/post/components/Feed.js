0998f44b182ad1762605cdad8c873b8c06f85943
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-25 21:38:41 +0200

    Formatted Feed component

--- frontend/src/modules/post/components/Feed.js
+++ frontend/src/modules/post/components/Feed.js
@@ -1,29 +1,31 @@
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Posts from './Posts';
 import * as selectors from '../selectors';
-import {Pager} from '../../common';
+import { Pager } from '../../common';
 import * as actions from '../actions';
 
 const Feed = () => {
-    const posts = useSelector(selectors.getPostSearch);
-    const dispatch = useDispatch();
+	const posts = useSelector(selectors.getPostSearch);
+	const dispatch = useDispatch();
 
-    if (!posts) {
-        return null;
-    }
+	if (!posts) {
+		return null;
+	}
 
-    return (
-        <div>
-            <Posts posts={posts.result.items}/>
-            <Pager
-                back={{
-                    enabled: posts.criteria.page >= 1,
-                    onClick: () => dispatch(actions.previousFindPostsResultPage(posts.criteria))}}
-                next={{
-                    enabled: posts.result.existMoreItems,
-                    onClick: () => dispatch(actions.nextFindPostsResultPage(posts.criteria))}}/>
-        </div>
-    );
+	return (
+		<div>
+			<Posts posts={posts.result.items} />
+			<Pager
+				back={{
+					enabled: posts.criteria.page >= 1,
+					onClick: () => dispatch(actions.previousFindPostsResultPage(posts.criteria))
+				}}
+				next={{
+					enabled: posts.result.existMoreItems,
+					onClick: () => dispatch(actions.nextFindPostsResultPage(posts.criteria))
+				}} />
+		</div>
+	);
 
 }
 
