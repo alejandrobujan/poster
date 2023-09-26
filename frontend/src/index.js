f9f2cd9e09bcb7c9484f4de1c6a4b381ca9bdffa
Martin Regueiro Golan <martin.regueiro1@udc.es>
2023-09-26 09:34:37 +0200

    Removed dependency fontawesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,8 +18,7 @@ import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
+
 
 
 import registerServiceWorker from './registerServiceWorker';
