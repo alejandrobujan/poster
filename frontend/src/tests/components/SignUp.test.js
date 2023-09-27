0a79c10816341c7e3e5b32b1633ce398c97a064f
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-09-27 23:19:33 +0200

    Modified SignUp frontend test

--- frontend/src/tests/components/SignUp.test.js
+++ frontend/src/tests/components/SignUp.test.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import renderer from "react-test-renderer";
 import store from "../../store";
 import { SignUp } from "../../modules/users";
+import '@testing-library/jest-dom'
 
 import {createMemoryHistory} from 'history';
 
@@ -20,6 +21,61 @@ describe("SignUp", () => {
 			)
 			.toJSON();
 		expect(tree).toMatchSnapshot();
+	});
+
+	it("registers correctly", async () => {
+
+
+		const history = createMemoryHistory();
+
+		render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<SignUp/>
+				</MemoryRouter>
+			</Provider>
+		);
+
+        const login = screen.getByLabelText('Login');
+
+        fireEvent.change(login, {
+			target: { value: "Pepe" },
+		});
+
+        const email = screen.getByLabelText('Email');
+
+        fireEvent.change(email, {
+			target: { value: "Pepe@pepe.com" },
+		});
+
+        const password = screen.getByLabelText('Password');
+
+        fireEvent.change(password, {
+			target: { value: "Pepe" },
+		});
+
+        const confirmPassword = screen.getByLabelText('Confirm password');
+
+        fireEvent.change(confirmPassword, {
+			target: { value: "Pepe" },
+		});
+
+        const firstName = screen.getByLabelText('First name');
+
+        fireEvent.change(firstName, {
+			target: { value: "Pepe" },
+		});
+
+        const lastName = screen.getByLabelText('Last name');
+
+        fireEvent.change(lastName, {
+			target: { value: "Pepe" },
+		});
+
+        const submit = screen.getByRole('button', {name: /submit/i});
+
+        fireEvent.click(submit);
+
 	});
 
 });
