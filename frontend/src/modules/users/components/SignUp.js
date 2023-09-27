09a5d103cf35427ea583249d904edc923f83535e
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-27 15:18:34 +0200

    Updated avatar file control in SignUp form

--- frontend/src/modules/users/components/SignUp.js
+++ frontend/src/modules/users/components/SignUp.js
@@ -23,6 +23,8 @@ const SignUp = () => {
 	const [passwordsDoNotMatch, setPasswordsDoNotMatch] = useState(false);
 	let form;
 	let confirmPasswordInput;
+	let avatarInput;
+	let clearAvatar;
 
 	const handleSubmit = event => {
 
@@ -77,6 +79,32 @@ const SignUp = () => {
 		confirmPasswordInput.setCustomValidity('');
 		setConfirmPassword(value);
 		setPasswordsDoNotMatch(false);
+
+	}
+
+	const handleAvatarChange = async e => {
+
+		const maxSize = 1024000;
+		const file = e.target.files[0];
+		
+		clearAvatar.style.display = file ? 'inline' : 'none';
+
+		if (file && file.size <= maxSize) {
+			setAvatar(await fileToBase64(file));
+			avatarInput.setCustomValidity('');
+		} else {
+			setAvatar('');
+			avatarInput.setCustomValidity(file ? 'error' : '');
+
+		}
+
+	}
+
+	const handleClearAvatar = () => {
+		
+		avatarInput.value = '';
+		avatarInput.setCustomValidity('');
+		clearAvatar.style.display = 'none';
 
 	}
 
@@ -186,9 +214,18 @@ const SignUp = () => {
 								Avatar
 							</label>
 							<div className="col-md-6">
-								<input type="file" id="avatar" accept="image/*"
-									onChange={async e => setAvatar(await fileToBase64(e.target.files[0]))}
+								<input ref={node => avatarInput = node} type="file" id="avatar" accept="image/*"
+									onChange={handleAvatarChange}
 								/>
+								<button ref={node => clearAvatar = node} type="button" className="btn btn-outline-danger" onClick={handleClearAvatar} style={{'display':'none'}}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+										<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+										<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+									</svg>
+								</button>
+								<div className="invalid-feedback">
+									The maximum size allowed is 1MB.
+								</div>
 							</div>
 						</div>
 						<div className='text-center'>
