7e9206603081e40ac7682893bac3a443e17173ff
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-09-28 02:30:51 +0200

    Added frontend test CreatePost try to post correctly. Not finished

--- frontend/src/tests/components/CreatePost.test.js
+++ frontend/src/tests/components/CreatePost.test.js
@@ -1,9 +1,13 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import {Provider} from 'react-redux';
 import renderer from "react-test-renderer";
 import store from "../../store";
 import {CreatePost} from "../../modules/post";
+import '@testing-library/jest-dom'
+
+import {createMemoryHistory} from 'history';
 
 describe("CreatePost", () => {
 
@@ -19,5 +23,48 @@ describe("CreatePost", () => {
 			.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
+
+	/*it("try to post correctly", async () => {
+
+
+		const history = createMemoryHistory();
+
+		render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<CreatePost/>
+				</MemoryRouter>
+			</Provider>
+		);
+
+		const title = screen.getByLabelText('Title');
+
+        fireEvent.change(title, {
+			target: { value: "Porsche cayenne" },
+		});
+
+		const description = screen.getByLabelText('Description');
+
+        fireEvent.change(description, {
+			target: { value: "The ideal car for you. Perfect for everything and for everyone." },
+		});
+
+		const url = screen.getByLabelText('Url');
+
+        fireEvent.change(url, {
+			target: { value: "https://www.porsche.com/international/models/cayenne/cayenne-models/cayenne/" },
+		});
+
+		const price = screen.getByLabelText('Price');
+
+        fireEvent.change(price, {
+			target: { value: "55000.05" },
+		});
+
+        const submit = screen.getByRole('button', {name: /submit/i});
+
+        fireEvent.click(submit);
+
+	});*/
 
 });
