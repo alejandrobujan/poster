ea2d7b3c405ea6c35676723f346791345f84d7fa
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-09-29 23:25:19 +0200

    Updated post links target

--- frontend/src/modules/post/components/Post.js
+++ frontend/src/modules/post/components/Post.js
@@ -21,7 +21,7 @@ const Post = ({ title, description, url, price, categoryDto, images }) => {
 						<hr className="my-4" />
 						<h5 className="fw-bolder">
 							{title} &nbsp; {url &&
-							<a className="btn btn-primary" href={url}>
+							<a className="btn btn-primary" href={url} target="_blank">
 								<div className="d-flex align-items-center">
 									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-link" viewBox="0 0 16 16">
 										<path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"></path>
