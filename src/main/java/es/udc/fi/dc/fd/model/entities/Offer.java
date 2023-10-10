ef486683182cf662126c53cb9863205bd3454bf6
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-10-10 13:12:42 +0200

    Added constructor for Offer entity

--- src/main/java/es/udc/fi/dc/fd/model/entities/Offer.java
+++ src/main/java/es/udc/fi/dc/fd/model/entities/Offer.java
@@ -1,5 +1,8 @@
 package es.udc.fi.dc.fd.model.entities;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 
 /**
@@ -8,11 +11,25 @@ import jakarta.persistence.Entity;
 @Entity
 public class Offer extends Post {
 
+	/**
+	 * @param title
+	 * @param description
+	 * @param url
+	 * @param price
+	 * @param creationDate
+	 * @param user
+	 * @param category
+	 */
+	public Offer(String title, String description, String url, BigDecimal price, LocalDateTime creationDate, User user,
+			Category category) {
+		super(title, description, url, price, creationDate, user, category);
+	}
+
 	@Override
 	public String toString() {
 		return "Offer [id=" + getId() + ", title=" + getTitle() + ", description=" + getDescription() + ", url="
 				+ getUrl() + ", price=" + getPrice() + ", creationDate=" + getCreationDate() + ", user=" + getUser()
-				+ ", category=" + getCategory() + ", images=" + getImages() + ", positiveRatings=" + getPositiveRatings()
-				+ ", negativeRatings=" + getNegativeRatings() + ", expired=" + isExpired() + "]";
+				+ ", category=" + getCategory() + ", images=" + getImages() + ", positiveRatings="
+				+ getPositiveRatings() + ", negativeRatings=" + getNegativeRatings() + ", expired=" + isExpired() + "]";
 	}
 }
