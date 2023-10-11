84749632287ae42cd250c04b79e2deb6da19deb0
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-10-11 13:46:40 +0200

    Applied strategy design pattern with handlers

---
+++ src/main/java/es/udc/fi/dc/fd/model/services/CouponHandler.java
@@ -0,0 +1,95 @@
+package es.udc.fi.dc.fd.model.services;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.udc.fi.dc.fd.model.common.exceptions.InstanceNotFoundException;
+import es.udc.fi.dc.fd.model.entities.Category;
+import es.udc.fi.dc.fd.model.entities.CategoryDao;
+import es.udc.fi.dc.fd.model.entities.Coupon;
+import es.udc.fi.dc.fd.model.entities.Image;
+import es.udc.fi.dc.fd.model.entities.ImageDao;
+import es.udc.fi.dc.fd.model.entities.Post;
+import es.udc.fi.dc.fd.model.entities.PostDao;
+import es.udc.fi.dc.fd.model.entities.User;
+import es.udc.fi.dc.fd.model.services.exceptions.MaximumImageSizeExceededException;
+import es.udc.fi.dc.fd.model.services.exceptions.MissingRequiredParameterException;
+
+/**
+ * 
+ */
+@Component
+@Transactional
+public class CouponHandler implements PostHandler {
+
+	@Autowired
+	private PermissionChecker permissionChecker;
+
+	/** The category dao. */
+	@Autowired
+	private CategoryDao categoryDao;
+
+	/** The post dao. */
+	@Autowired
+	private PostDao postDao;
+
+	/** The image dao. */
+	@Autowired
+	private ImageDao imageDao;
+
+	@Override
+	public Post handleCreate(String title, String description, String url, BigDecimal price, Long userId,
+			Long categoryId, List<byte[]> imageList, Map<String, String> properties)
+			throws InstanceNotFoundException, MaximumImageSizeExceededException, MissingRequiredParameterException {
+
+		User user = permissionChecker.checkUser(userId);
+
+		Category category = null;
+
+		if (categoryId != null) {
+			Optional<Category> categoryOptional = categoryDao.findById(categoryId);
+
+			if (!categoryOptional.isPresent())
+				throw new InstanceNotFoundException("project.entities.category", categoryId);
+
+			category = categoryOptional.get();
+		}
+
+		LocalDateTime creationDate = LocalDateTime.now();
+
+		String code = properties.get("code");
+
+		if (code == null) {
+			throw new MissingRequiredParameterException("code", getClass().getSimpleName());
+		}
+
+		Post post = postDao.save(new Coupon(title, description, url, price, creationDate, code, user, category));
+
+		int maxSize = 1024000;
+		Image image;
+
+		for (byte[] imageBytes : imageList) {
+
+			image = new Image(imageBytes, post);
+
+			if (image.getData().length > maxSize) {
+				throw new MaximumImageSizeExceededException(maxSize);
+			}
+
+			post.addImage(image);
+			imageDao.save(image);
+
+		}
+
+		return post;
+
+	}
+
+}
