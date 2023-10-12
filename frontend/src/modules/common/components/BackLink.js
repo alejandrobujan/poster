40b5a8263b4f6a702898f3b8df1520a5d09b7481
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-10-12 17:33:31 +0200

    Modified back link with icon

--- frontend/src/modules/common/components/BackLink.js
+++ frontend/src/modules/common/components/BackLink.js
@@ -1,19 +1,19 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const BackLink = () => {
 
-    const navigate = useNavigate();
-    
-    return (
+	const navigate = useNavigate();
 
-        <button type="button" className="btn btn-link" 
-            onClick={() => navigate(-1)}>
+	return (
 
-            Back
+		<button type="button" class="btn btn-primary" onClick={() => navigate(-1)}>
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+				<path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"></path>
+			</svg>
+		</button>
 
-        </button>
 
-    );
+	);
 
 };
 
