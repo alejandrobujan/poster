19021521bee70cae4dbc28fc4b2139d24e9fa4ea
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-10-13 12:40:15 +0200

    Modified user card layout

--- frontend/src/modules/common/components/UserCard.js
+++ frontend/src/modules/common/components/UserCard.js
@@ -1,26 +1,18 @@
 const UserCard = ({ user }) => {
-    return (
-        <div className="card border mb-3" style={{ maxWidth: "10rem", borderRadius: "12px" }}>
-            <div className="row no-gutters">
-                <div className="col-md-4">
-                    <img
-                        src={user.avatar ? `data:image/*;base64,${user.avatar}` : "/poster/assets/profile.png"}
-                        alt="Avatar"
-                        className="card-img"
-                        style={{ borderRadius: "12px 0 0 12px" }}
-                    />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body text-center p-2">
-                        <h6 className="card-title font-weight-bold mb-1">{user.userName}</h6>
-                        <p className="card-text mb-0">
-                            <i>{`${user.firstName + " " + user.lastName}`}</i>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div class="user-card">
+			<img
+				src={user.avatar ? `data:image/*;base64,${user.avatar}` : "/poster/assets/profile.png"}
+				alt="Avatar"
+				width="30px"
+				height="30px"
+			/>
+			<div>
+				<div><b>{`${user.userName}`}</b></div>
+				<div><i>{`${user.firstName + " " + user.lastName}`}</i></div>
+			</div>
+		</div>
+	);
 };
 
 export default UserCard;
