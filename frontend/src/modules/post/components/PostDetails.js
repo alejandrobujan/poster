0a7d91b3e7ee5fd31bc778844bd6afee1b7acd44
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-10-14 13:24:01 +0200

    Modified PostDetails.js for accessing PostUpdate

--- frontend/src/modules/post/components/PostDetails.js
+++ frontend/src/modules/post/components/PostDetails.js
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams , useNavigate } from 'react-router-dom';
 
 import * as selectors from '../selectors';
+import * as userSelectors from '../../users/selectors';
 import * as actions from '../actions';
 import { BackLink, UserCard } from '../../common';
 import { getDate } from '../../../backend/utils';
@@ -13,8 +14,11 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const PostDetails = () => {
 	const post = useSelector(selectors.getPost);
+	const user = useSelector(userSelectors.getUser);
+	const userName = useSelector(userSelectors.getUserName);
 	const { id } = useParams();
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 
@@ -72,6 +76,18 @@ const PostDetails = () => {
 							&nbsp;
 							{post.negativeRatings}
 						</button>
+						&nbsp;
+						{userName!=null && (user.id === post.userSummaryDto.id) &&
+							<a className="btn btn-primary" onClick={() => navigate(`/post/post-update/${id}`)} target="_blank">
+								<div className="d-flex align-items-center">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+									  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+									  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+									</svg>
+									<span className="ml-2">Modify post</span>
+								</div>
+							</a>
+						}
 					</div>
 
 				</div>
