6f44c0e3265f55445e3f3b673975ea4522b4e9e1
JavierFreireBouzas <javier.freire.bouzas@udc.es>
2023-10-17 00:20:59 +0200

    Modified ImagesCard.js for having a better representation

--- frontend/src/modules/common/components/ImagesCard.js
+++ frontend/src/modules/common/components/ImagesCard.js
@@ -7,16 +7,19 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const ImagesCard = ( {images} ) => {
-	//const postImages = useSelector(selectors.getImages);
 	return (
-		<div className="images-card">
-			<p>Uploaded images: </p>
+		<div>
+			<p>Uploaded images:</p>
+				{images.length === 0 ?
+						(<p className='noImages'>
+							No uploaded images.
+						</p>):(
+							<div className='images-card'>
+								<ImageGallery showPlayButton={false} showFullscreenButton={false}
+								items={images.map((image) => ({ original: `data:image/*;base64,${image}`, thumbnailHeight: "30px" }))} />
+							</div>)
+				}
 			
-			{images.length === 0 ?
-							(<ImageGallery showFullscreenButton={false} showPlayButton={false} showNav={false}
-								items={[{ original: '/poster/assets/noimage.png', originalHeight: "30px" }]} />) :
-							(<ImageGallery showPlayButton={false} showFullscreenButton={false}
-								items={images.map((image) => ({ original: `data:image/*;base64,${image}`, thumbnailHeight: "30px" }))} />)}
 		</div>
 	);
 };
