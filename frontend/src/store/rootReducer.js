4f98eec9e94fcde006aa7b7ed54087dabd14ba2b
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-10-18 14:19:09 +0200

    Added catalog reducer inside rootReducer

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import app from '../modules/app';
 import users from '../modules/users';
 import post from '../modules/post';
+import catalog from '../modules/catalog';
 
 const rootReducer = combineReducers({
     app: app.reducer,
     users: users.reducer,
-    post: post.reducer
+    post: post.reducer,
+    catalog: catalog.reducer
 });
 
 export default rootReducer;
