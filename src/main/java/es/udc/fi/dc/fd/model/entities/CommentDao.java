026fb5637d575f344f10725ad30bc355de40d8ca
Martin Regueiro Golan <martin.regueiro1@udc.es>
2023-11-10 13:52:23 +0100

    Created CommentDao and Entity. This commit references #2389 @0h15m

---
+++ src/main/java/es/udc/fi/dc/fd/model/entities/CommentDao.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.fd.model.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface CommentDao.
+ */
+public interface CommentDao extends JpaRepository<Comment, Long> {
+
+}
