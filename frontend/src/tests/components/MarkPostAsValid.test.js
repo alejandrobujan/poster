8d6380b09f69cefcd9f028baa12e56c5155ed8a8
Patricia Mato Miragaya <patricia.mato.miragaya@udc.es>
2023-11-15 20:00:18 +0100

    Fixed merge conflicts with develop. This commit refs #2492 @0h42m

--- frontend/src/tests/components/MarkPostAsValid.test.js
+++ frontend/src/tests/components/MarkPostAsValid.test.js
@@ -6,61 +6,13 @@ import { MemoryRouter } from "react-router-dom";
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import PostDetails from '../../modules/post/components/PostDetails';
-
+import { postMock } from "../state/Post.mock";
 
 describe('PostDetails Component', () => {
 	const middlewares = [thunk];
     const mockStore = configureMockStore(middlewares);
     let store;
-    
-    const postMock = {
-    users: {
-        user: {
-            serviceToken: "923u2344823903480923",
-            userDto: {
-                id: 1,
-                userName: "Pepe",
-                password: "Pepe",
-                firstName: "Pepe",
-                lastName: "Pepe",
-                email: "Pepe@pepe.com",
-                avatar: null
-            }
-        }
-    },
-    post:{
-        post: {
-            id: 1,
-            title: "Coupon",
-            description: "Buena oferta",
-            url: "http://cupon",
-            price: 10,
-            categoryDto: {
-                id: 1,
-                name: "Belleza"
-            },
-            userSummaryDto: {
-                id: 1,
-                userName: "Pepe",
-                firstName: "Pepe",
-                lastName: "Pepe",
-                avatar: null
-            },
-            images: [],
-            creationDate: 1384723847238,
-            positiveRatings: 0,
-            negativeRatings: 0,
-            expired: false,
-            validationDate:null,
-            type: "Coupon",
-            properties: {
-                code: "12345"
-            }
-        }
-    },
-}
-
-store = mockStore(postMock);
+	store = mockStore(postMock);
     
   it("renders correctly", () => {
 		const tree = renderer
