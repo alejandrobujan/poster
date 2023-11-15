af63e7fa8c159da965456555ccf15879616c9bf0
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-11-15 02:12:05 +0100

    Added frontend tests for users module

---
+++ frontend/src/tests/mocks/User.mock.js
@@ -0,0 +1,16 @@
+export const userMock = {
+    users: {
+        user: {
+            serviceToken: "923u2344823903480923",
+            userDto: {
+                id: 1,
+                userName: "Pepe",
+                password: "Pepe",
+                firstName: "Pepe",
+                lastName: "Pepe",
+                email: "Pepe@pepe.com",
+                avatar: null
+            }
+        }
+    },
+}
