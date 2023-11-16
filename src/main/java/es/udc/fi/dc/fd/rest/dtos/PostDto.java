8005229113fe7cc0628e48ebf3c1acceaf69e8a2
Alejandro Buján Pampín <alejandro.bujan.pampin@udc.es>
2023-11-16 13:26:08 +0100

    Updated PostDto. This commit refs #2499 @0h13m

--- src/main/java/es/udc/fi/dc/fd/rest/dtos/PostDto.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/PostDto.java
@@ -2,17 +2,16 @@ package es.udc.fi.dc.fd.rest.dtos;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * The class PostDto
  */
 @Getter
-@AllArgsConstructor
 public class PostDto {
 	/** The post id. */
 	private Long id;
@@ -44,6 +43,43 @@ public class PostDto {
 	private String type;
 	/** The properties of the post */
 	private Map<String, String> properties;
+
+	/**
+	 * @@param id the id
+	 * @param title           the title of the post
+	 * @param description     the description of the post
+	 * @param url             the url associated to the post
+	 * @param price           the price of the post
+	 * @param categoryDto     the category dto associated to the post
+	 * @param userCreatorDto  the user summary dto from the user who create the post
+	 * @param images          the images associated to the post
+	 * @param creationDate    the creation date of the post
+	 * @param positiveRatings the number of positive ratings of the post
+	 * @param negativeRatings the number of negative ratings of the post
+	 * @param expired         if the post is expired or not
+	 * @param validationDate  the validation date
+	 * @param type            the post type
+	 * @param properties      the properties of the post
+	 */
+	public PostDto(Long id, String title, String description, String url, BigDecimal price, CategoryDto categoryDto,
+			UserSummaryDto userCreatorDto, List<byte[]> images, Long creationDate, int positiveRatings,
+			int negativeRatings, boolean expired, Long validationDate, String type, Map<String, String> properties) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.url = url;
+		this.price = price;
+		this.categoryDto = categoryDto;
+		this.userSummaryDto = userCreatorDto;
+		this.images = new ArrayList<>(images);
+		this.creationDate = creationDate;
+		this.positiveRatings = positiveRatings;
+		this.negativeRatings = negativeRatings;
+		this.expired = expired;
+		this.validationDate = validationDate;
+		this.type = type;
+		this.properties = new HashMap<>(properties);
+	}
 
 	public PostDto() {
 	}
