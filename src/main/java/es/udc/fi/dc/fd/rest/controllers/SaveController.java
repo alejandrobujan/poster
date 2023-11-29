8b0c758d8f809113eee4d0ccb2c9f364dc2616ab
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-11-29 12:04:03 +0100

    Adapted controllers to admit delete notifications. This commit refs #2586 @0h38m

--- src/main/java/es/udc/fi/dc/fd/rest/controllers/SaveController.java
+++ src/main/java/es/udc/fi/dc/fd/rest/controllers/SaveController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,7 +53,7 @@ public class SaveController {
 		saveService.savePost(postId, userId);
 	}
 
-	@PostMapping("/post/{postId}/save")
+	@GetMapping("/post/{postId}/save")
 	public boolean isPostSavedByUser(@RequestAttribute Long userId, @PathVariable Long postId) {
 		return saveService.isPostSavedByUser(postId, userId);
 	}
--- src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
@@ -9,7 +9,7 @@ public class NotificationConversor {
 
 	public final static NotificationDto toNotificationDto(Notification notification) {
 		return new NotificationDto(notification.getId(), notification.getText(), notification.isViewed(),
-				notification.getCreationDate(), notification.getPost().getId(),
+				notification.getCreationDate(), notification.getPost() != null ? notification.getPost().getId() : null,
 				new UserSummaryDto(notification.getNotifierUser().getId(), notification.getNotifierUser().getUserName(),
 						notification.getNotifierUser().getFirstName(), notification.getNotifierUser().getLastName(),
 						notification.getNotifierUser().getAvatar()));
