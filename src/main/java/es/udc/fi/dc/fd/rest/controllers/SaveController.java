68cc57f43a611ac0e6f377fd986e0602c282d030
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-11-29 12:19:08 +0100

    Modified save controller to throw exception. This commit refs #2586 @0h5m

--- src/main/java/es/udc/fi/dc/fd/rest/controllers/SaveController.java
+++ src/main/java/es/udc/fi/dc/fd/rest/controllers/SaveController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.udc.fi.dc.fd.model.common.exceptions.InstanceNotFoundException;
 import es.udc.fi.dc.fd.model.services.SaveService;
 import es.udc.fi.dc.fd.model.services.exceptions.AlreadySavedException;
+import es.udc.fi.dc.fd.model.services.exceptions.SavePostUserCreatorException;
 import es.udc.fi.dc.fd.rest.common.ErrorsDto;
 
 @RestController
@@ -26,6 +27,9 @@ public class SaveController {
 
 	/** The Constant ALREADY_SAVED_EXCEPTION_CODE. */
 	private static final String ALREADY_SAVED_EXCEPTION_CODE = "project.exceptions.AlreadySavedException";
+
+	/** The Constant SAVE_POST_USER_CREATOR_EXCEPTION_CODE. */
+	private static final String SAVE_POST_USER_CREATOR_EXCEPTION_CODE = "project.exceptions.SavePostUserCreatorException";
 
 	/** The message source. */
 	@Autowired
@@ -47,9 +51,21 @@ public class SaveController {
 
 	}
 
+	@ExceptionHandler(SavePostUserCreatorException.class)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	@ResponseBody
+	public ErrorsDto handleSavePostUserCreatorException(SavePostUserCreatorException exception, Locale locale) {
+
+		String errorMessage = messageSource.getMessage(SAVE_POST_USER_CREATOR_EXCEPTION_CODE, null,
+				SAVE_POST_USER_CREATOR_EXCEPTION_CODE, locale);
+
+		return new ErrorsDto(errorMessage);
+
+	}
+
 	@PostMapping("/post/{postId}")
 	public void savePost(@RequestAttribute Long userId, @PathVariable Long postId)
-			throws InstanceNotFoundException, AlreadySavedException {
+			throws InstanceNotFoundException, AlreadySavedException, SavePostUserCreatorException {
 		saveService.savePost(postId, userId);
 	}
 
