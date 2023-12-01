20df091ca0a9e55f20bfb147bcd96754d70cee80
Martin Regueiro Golan <martin.regueiro1@udc.es>
2023-12-01 11:44:58 +0100

    Modified PageNotFound. This commit refs #2587 @0h7m

--- frontend/src/modules/common/components/PageNotFound.js
+++ frontend/src/modules/common/components/PageNotFound.js
@@ -1,10 +1,14 @@
 const PageNotFound = () => {
-	return(
-		<div className="container text-center">
-		<h1 >404</h1>
-		<p>Page not found</p>
+	return (
+		<div className="container mt-5">
+			<div className="row">
+				<div className="col md-8 mx-auto text-center">
+					<h1 className="display-4">404 - Page Not Found</h1>
+					<p className="text-secondary">The page that you are looking for doesn't exists.</p>
+				</div>
+			</div>
 		</div>
-		
+
 	);
 };
 
