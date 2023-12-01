f4cb133adf5a6dc8bf927b36e6ece2092afa6a4a
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2023-12-01 10:43:09 +0100

    Modified conversor to admit notification sender null

--- src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
@@ -10,9 +10,10 @@ public class NotificationConversor {
 	public final static NotificationDto toNotificationDto(Notification notification) {
 		return new NotificationDto(notification.getId(), notification.getText(), notification.isViewed(),
 				notification.getCreationDate(), notification.getPost() != null ? notification.getPost().getId() : null,
-				new UserSummaryDto(notification.getNotifierUser().getId(), notification.getNotifierUser().getUserName(),
-						notification.getNotifierUser().getFirstName(), notification.getNotifierUser().getLastName(),
-						notification.getNotifierUser().getAvatar()));
+				notification.getNotifierUser() != null ? new UserSummaryDto(notification.getNotifierUser().getId(),
+						notification.getNotifierUser().getUserName(), notification.getNotifierUser().getFirstName(),
+						notification.getNotifierUser().getLastName(), notification.getNotifierUser().getAvatar())
+						: null);
 	}
 
 	public final static List<NotificationDto> toNotificationsDto(List<Notification> notifications) {
